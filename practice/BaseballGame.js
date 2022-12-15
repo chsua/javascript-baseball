73c5635f4ae01d078c0d0156764bf96eac1769aa
chsua <csj1919@naver.com>
2022-12-15 14:20:34 +0900

    [feat] userBall 예외처리 만들기

--- practice/BaseballGame.js
+++ practice/BaseballGame.js
@@ -16,6 +16,32 @@ class BaseballGame {
         return Random.pickUniqueNumbersInRange(1, 9, 3);
     }
 
+    requestUserBall(){
+        Console.readLine(MESSAGE.REQUEST_BASEBALL, userBall => {
+
+        })
+    }
+
+    userBallCallback(userBall){
+        try {
+            this.userBallValidate(userBall) ;
+
+        } catch (error) {
+
+        }
+    }
+    
+    userBallValidate(userBall){
+        if (userBall.length !== BASEBALL.RANGE ) throw new Error (MESSAGE.ERROR_BALL_RANGE) ;
+        if (userBall.includes(BASEBALL.EXCEPT)) throw new Error (MESSAGE.ERROR_BALL_EXCEPT) ;
+        userBall.forEach(num => {
+            if (!Number.isInteger(+num)) throw new Error (MESSAGE.ERROR_BALL_RANGE) ; 
+        });
+    }
+
+
 }
+
+
 
 module.exports = BaseballGame ;
