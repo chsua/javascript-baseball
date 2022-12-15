b58fda37e1ffeedfcc896b454da1f2794d60f95c
chsua <csj1919@naver.com>
2022-12-15 14:32:27 +0900

    [feat] userBall 과 correctBall 비교함수, 비교 결과 프린트 문구 생성함수

--- practice/BaseballGame.js
+++ practice/BaseballGame.js
@@ -25,20 +25,39 @@ class BaseballGame {
     userBallCallback(userBall){
         try {
             this.userBallValidate(userBall) ;
+            this.checkUserBallCorrectBall(userBall) ;
 
         } catch (error) {
-
+            this.requestUserBall() ;
         }
     }
     
     userBallValidate(userBall){
         if (userBall.length !== BASEBALL.RANGE ) throw new Error (MESSAGE.ERROR_BALL_RANGE) ;
         if (userBall.includes(BASEBALL.EXCEPT)) throw new Error (MESSAGE.ERROR_BALL_EXCEPT) ;
+        let countCheckSet = new Set() ;
         userBall.forEach(num => {
+            countCheckSet.add(num) ;
             if (!Number.isInteger(+num)) throw new Error (MESSAGE.ERROR_BALL_RANGE) ; 
         });
+        if (countCheckSet.size !== 3)  throw new Error (MESSAGE.ERROR_BALL_RANGE) ; 
     }
 
+    checkUserBallCorrectBall(userBall){
+        let ballStrikeCount = [0,0]
+        for (let i = 0 ; i < 3 ; i++ ){
+            if (this.#correctBall[i] === +userBall[i]) ballStrikeCount[1]++ ;
+            else if (this.#correctBall.includes(+userBall[i])) ballStrikeCount[0]++ ;
+        }
+        return ballStrikeCount ;
+    }
+
+    printResult([ball, strike]){
+        if ( ball === 0 && strike === 0 ) return MESSAGE.BALL[2] ;
+        if ( ball === 0 ) return `${strike}${MESSAGE.BALL[1]}`
+        if ( strike === 0 ) return `${ball}${MESSAGE.BALL[0]}`
+        return `${ball}${MESSAGE.BALL[0]} ${strike}${MESSAGE.BALL[1]}`
+    }
 
 }
 
